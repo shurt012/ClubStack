4ae6f76bd09d51cbb06d54783e9eb2a546af765c
oalme004@fiu.edu <oalme004@fiu.edu>
2017-04-03 16:00:43 -0400

    register and signin works now

---
+++ client/templates/register/register.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Caciano on 4/3/2017.
+ */
+
+Template.register.onRendered( () => {
+    function validatePass() {
+        if(document.getElementById("signinPass").value != document.getElementById("Confirm").value)
+            document.getElementById("Confirm").setCustomValidity("Passwords must match!");
+        else
+            document.getElementById("Confirm").setCustomValidity("");
+    }
+    document.getElementById("Confirm").onchange = validatePass;
+    document.getElementById("signinPass").onchange = validatePass;
+});
+
+Template.register.events({
+    "submit form": (event) => {
+        event.preventDefault();
+        document.getElementById("alerts").style.display = "none";
+        const firstName = event.target.signinFirst.value;
+        const lastName = event.target.signinLast.value;
+        const email = event.target.signinEmail.value;
+        const password = event.target.signinPass.value;
+        Accounts.createUser({
+            email: email,
+            password: password,
+            firstname: firstName,
+            lastname: lastName
+        }, (err, res) => {
+            if(err) {
+                document.getElementById("alerts").innerHTML = "<div><Strong>An account with this email already exists!</Strong></div>";
+                document.getElementById("alerts").style.display = "inherit";
+            }
+            else
+                FlowRouter.go("/");
+        });
+    }
+});
+
---
+++ client/templates/signin/signin.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Caciano on 4/3/2017.
+ */
+
+Template.signin.events({
+    'submit form':function(evt){
+        evt.preventDefault();
+        document.getElementById("alerts").style.display = "none";
+        const email = evt.target.signinEmail.value;
+        const password = evt.target.signinPass.value;
+        Meteor.loginWithPassword(email,password, (err, res) => {
+            if(!err)
+                FlowRouter.go('/');
+            else {
+                document.getElementById("alerts").innerHTML = "<div><strong>Log in credentials is incorrect! Please try again.</strong></div>";
+                document.getElementById("alerts").style.display = "inherit";
+            }
+        });
+    }
+});
