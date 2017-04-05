8c2ec59fa6486bf85d79aa18318fa8838be24ba7
oalme004@fiu.edu <oalme004@fiu.edu>
2017-04-05 16:50:36 -0400

    added search to home page

--- server/methods/accounts.js
+++ server/methods/accounts.js
@@ -12,9 +12,6 @@ Accounts.onCreateUser( (options, user) => {
 Accounts.validateNewUser( (user) => {
     return (/.*@fiu\.edu$/).test(user.emails[0].address) ? true : (() => {throw new Meteor.Error("Invalid Email", "Please use an FIU email.")});
 });
-Accounts.validateNewUser( (user) => {
-    return (/.*@fiu\.edu$/).test(user.emails[0].address) ? true : (() => {throw new Meteor.Error("Invalid Email", "Please use an FIU email.")});
-});
 
 Meteor.methods({
     AssignRole: function() {
