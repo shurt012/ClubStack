950de11cd37d1190daaf57e436135b492c4c82b4
oalme004@fiu.edu <oalme004@fiu.edu>
2017-04-13 12:36:33 -0400

    fixed user.clubs bug

--- server/methods/club.js
+++ server/methods/club.js
@@ -6,8 +6,12 @@ import Club from "/both/collections/club";
 
 Meteor.methods({
     clubOwner: function() {
-        const club = Club.findOne( {admin: this.userId} );
-        Meteor.users.update({_id: this.userId}, { $push: {clubs: club["Club Name"]} });
+        let updatedArray = [];
+        const updatedClubs = Club.find( {admin: this.userId}, {fields: {"Club Name": 1, _id: 0}} ).fetch();
+        for(let i = 0; i < updatedClubs.length; i++)
+            updatedArray.push(updatedClubs[i]["Club Name"]);
+
+        Meteor.users.update({_id: this.userId}, { $set: {clubs: updatedArray} });
     },
     insertClub: function(doc) {
         Club.insert(doc);
